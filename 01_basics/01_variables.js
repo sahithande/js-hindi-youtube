2c7e6d2e28f391a4dbdb80f8e6cd30cce4261044
Sahith Ande <143463487+sahithande@users.noreply.github.com>
2025-02-09 07:22:34 +0000

    added variables file

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 14453;
+let accountEmail = "sahithand.55@gmail.com";
+var accountPassword = "12345";
+accountCity = "khammam";
+
+let accountState;
+
+//accountId = 12232;
+
+accountEmail = "sahithande10@gmail.com";
+accountPassword = "54321";
+accountCity = "hyderabad"
+console.table([accountEmail, accountPassword, accountState, accountCity, accountId]);
